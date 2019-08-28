1edc5b01c7c6f5e09556e5ded848d93c82c195c1
Patrick O <patrick.odherty@gmail.com>
2019-08-28 19:46:13 -0400

    added some logic for drawing lines

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
+var id = 0;
+
+var a = '';
+var b = '';
+
+var graph = {};
+
+graph['example'] = {'posX': 500, 'posY' : 500, 'next': [0, 1, 2, 3, 5]};
+graph['example']['next'].push(6);
+
+console.log(graph);
+
 var currentMode = 'edit';
 
 $(document).ready(function() {
@@ -5,7 +17,10 @@ $(document).ready(function() {
         if (event.target.id == 'game') {
             const game = document.getElementById('game');
             let dot = document.createElement('span');
+            graph[id] = {'posX' : event.pageX, 'posY': event.pageY, 'next': []};
+            console.log(graph);
             dot.className = 'dot';
+            dot.id = id++;
             dot.style.top = event.pageY - 10;
             dot.style.left = event.pageX - 10;
             game.appendChild(dot);
@@ -13,7 +28,24 @@ $(document).ready(function() {
             if (currentMode == 'edit') {
                 $(event.target).remove();
             } else if (currentMode == 'line') {
-
+                console.log(event.target);
+                if (a == '') {
+                    a = event.target.id;
+                } else if (b == '') {
+                    //check if b is already in a's 'next' array
+                    b = event.target.id;
+                    for (let node of graph[a]['next']) {
+                        if (node == b || node == a) {
+                            return;
+                        }
+                    }
+                    //connect dots and make lines and update 'next' array
+                    console.log('line between ' + a + ' and ' + b);
+                    graph[a]['next'].push(b);
+                    graph[b]['next'].push(a);
+                    a = ''; 
+                    b = '';
+                }
             }
         }
     });
